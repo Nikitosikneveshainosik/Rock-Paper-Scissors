37c7fc3b7adbe1bb500c04f55a355d130c438b7d
Nikita <nikitamironov442@gmail.com>
2021-05-22 01:16:46 +0700

    Улучшен подсчёт статистики, добавлен выход из игры по желанию игрока

--- Main.py
+++ Main.py
@@ -3,13 +3,18 @@ draw = 0
 win_player = 0
 win_comp = 0
 Mas = []
+player = 1
+x = 0
 
-for x in range(0, 10, 1):
+while player != 0:
     comp = random.randint(1, 3)
     player = int(input("""    1 - Ножницы
     2 - Бумага
     3 - Камень
+    0 - Закончить игру
 Выберите свою сторону: """))
+    if player == 0:
+        break
 
     if player > 3 or player < 1:
         print("Наверное, в вас прилетел камень.\nВ этот раз введите правильное число: ")
@@ -37,11 +42,17 @@ for x in range(0, 10, 1):
         win_player += 1
         Mas.append('Вы победили')
         print('You Win!')
+    x += 1
 
 print('\t', '/'*5, 'Результаты игры', '/'*5)
-for i in range(0, 10, 1):
+for i in range(0, x, 1):
     print('\tВ', i + 1, 'раунде', Mas[i])
 print('\n')
-print(win_comp, "побед у компьютера -", win_comp * 10, '%')
-print(win_player, "побед у игрока -", win_player * 10, '%')
-print(draw, "ничьих -", draw * 10, '%')
+
+winning_percentage_player = round(float((win_player/x) * 100), 1)
+winning_percentage_comp = round(float((win_comp/x) * 100), 1)
+draw_percentage = round(float((draw/x) * 100), 1)
+
+print(win_comp, "побед у компьютера -", winning_percentage_comp, '%')
+print(win_player, "побед у игрока -", winning_percentage_player, '%')
+print(draw, "ничьих -", draw_percentage, '%')
